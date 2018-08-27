c01fe097f67e20d4324bdb08ae580a567b445374
Vladas Zakrevskis <146100@gmail.com>
2018-08-27 13:00:43 +0300

    removed dependencies

--- init.el
+++ init.el
@@ -2,13 +2,11 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,16 +15,15 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(custom-enabled-themes (quote (misterioso)))
- '(package-selected-packages (quote (flycheck-rtags flycheck cmake-ide)))
- '(visible-bell nil))
+ '(default-frame-alist (quote ((height . 80) (width . 200))))
+ '(package-selected-packages (quote (treemacs flycheck-rtags flycheck cmake-ide))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(require 'rtags)
-(cmake-ide-setup)
 
 (defcustom al-path "~/.emacs.d/init.el"
    "init.el file path."
@@ -37,4 +34,4 @@
 (defun al ()
   "Open init.el file"
   (interactive)
-  (find-file-other-window al-path))
+  (find-file al-path))
