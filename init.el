caf2b3b97d2e703aab59a7254fba7dd5ffc69837
Vladas Zakrevskis <146100@gmail.com>
2018-08-30 00:28:59 +0300

    shortcuts file

--- init.el
+++ init.el
@@ -18,7 +18,8 @@
  '(default-frame-alist (quote ((height . 80) (width . 160))))
  '(package-selected-packages
    (quote
-    (slime use-package treemacs flycheck-rtags flycheck cmake-ide)))
+    (cmake-mode magit auto-complete use-package treemacs)))
+ '(treemacs-width 22)
  '(truncate-lines nil))
 
 (custom-set-faces
@@ -26,19 +27,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo")))))
+ '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :height 160 :family "Menlo")))))
 
-(defcustom al-path "~/.emacs.d/init.el"
-   "init.el file path."
-   :group 'paths
-   :type 'directory
-)
 
-(defun al ()
-  "Open init.el file"
-  (interactive)
-  (find-file al-path))
+(add-to-list 'load-path "~/.emacs.d/init")
 
-(defalias 'inst 'package-install)
-(defalias 'var  'customize-variable)
-(defalias 'eb   'eval-buffer)
+(load "shortcuts")
+
